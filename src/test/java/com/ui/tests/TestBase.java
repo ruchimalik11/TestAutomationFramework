d65bc74b2438a9636993b457abacfaf15bfbd2cb
ruchimalk <ruchimalik11@gmail.com>
2026-08-23 22:03:06 +0100

    Updated TestBase-Added After Annotation

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -46,7 +47,7 @@ public class TestBase {
 	public BrowserUtility getInstance() {
 		return homePage;
 	}
-
+@AfterMethod
 	public void tearDown() {
 		
 		if(isLambdaTest) {
